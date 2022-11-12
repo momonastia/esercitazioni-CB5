6e9676b6e43786ea9ded19fc615698ffccb5bda8
momonastia~ <anastasiatyrikova@gmail.com>
2022-11-12 09:23:04 +0100

    esercitazione 11-11-22

--- 11-11-22/script.js
+++ 11-11-22/script.js
@@ -35,38 +35,58 @@ const mainDiv = document.querySelector(".main-div");
 const btnPrevious = document.querySelector(".btn-previous");
 const btnNext = document.querySelector(".btn-next");
 
-let counter = 0;
-const Previous = (num) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
-    .then((res) => res.json())
-    .then((res) =>
-      createCard(
-        res.name,
-        res.id,
-        res.types[0].type.name,
-        res.sprites.back_default,
-        mainDiv
-      )
-    )
-    .finally();
+// Handle Paginated Cards
+
+const LIMIT = 10;
+let page = 1;
+
+/**
+ * It takes a number, and returns a promise that resolves to a JSON object containing the data for that
+ * pokemon
+ * @param num - The number of the pokemon you want to fetch.
+ */
+const getPokemon = (num) =>
+  fetch(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res) => res.json());
+
+const addPokemonCard = (pokemon) =>
+  createCard(
+    pokemon.name,
+    pokemon.id,
+    pokemon.types[0].type.name,
+    pokemon.sprites.back_default,
+    mainDiv
+  );
+
+/**
+ * It fetches a new page of Pokemon, adds the Pokemon to the page, and then updates the page number
+ * @param newPage - The new page number to update to.
+ */
+const updatePage = (newPage) => {
+  /* Checking if the newPage is greater than 0 and less than or equal to the limit. */
+  if (newPage > 0 && newPage <= LIMIT) {
+    getPokemon(newPage)
+      .then(addPokemonCard)
+      .then(() => (page = newPage));
+  }
 };
 
-const Next = (num) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
-    .then((res) => res.json())
-    .then((res) =>
-      createCard(
-        res.name,
-        res.id,
-        res.types[0].type.name,
-        res.sprites.back_default,
-        mainDiv
-      )
-    );
-};
+/**
+ *  Async/Await version
+ */
 
-btnNext.addEventListener("click", () => FetchPockemonCard(counter++));
-btnPrevious.addEventListener("click", () => FetchPockemonCard(counter--));
+// const updatePage = async (newPage) => {
+//   if (newPage > 0 && newPage <= LIMIT) {
+//     const respose = await fetchPokemon(newPage);
+//     getPokemon(respose);
+//     page = newPage;
+//   }
+// };
+
+btnNext.addEventListener("click", () => updatePage(page + 1));
+btnPrevious.addEventListener("click", () => updatePage(page - 1));
+
+/* Calling the updatePage function with the argument of 1 to get the first pokemon card */
+updatePage(page);
 
 /// ID format with 0
 
